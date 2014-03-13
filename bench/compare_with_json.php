158e9c38318e0505b3977b54880101e48b8fb746
Jakub Zelenka <bukka@php.net>
2014-03-13 18:53:48 +0000

    Improve bench a bit

--- bench/compare_with_json.php
+++ bench/compare_with_json.php
@@ -14,7 +14,7 @@ function test_json($json) {
 	$end = microtime(true);
 
 	$duration = $end - $start;
-	printf("JSON: time for %d iterations: %f" . PHP_EOL, $iters, $duration);
+	printf("JSON:  time for %d iterations: %f" . PHP_EOL, $iters, $duration);
 }
 
 
@@ -34,10 +34,10 @@ function test_jsond($json) {
 
 $jsons = array(
 	'{"i": 23, "array": [1, null, false, true, ["aha", "baba", 23, {"test": 23}]]}',
-	'"haha"',
-	'{"a": 23}',
-	'[[[[[1]]]]]',
-	'{"long-str": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, \n omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."}'
+	'[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]',
+	'{"a": 23.2234232}',
+	'{"long-str": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.", }',
+	json_encode($_SERVER),
 );
 
 foreach ($jsons as $json) {
