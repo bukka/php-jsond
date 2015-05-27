131e1ea685fb234c97bf5db78f514404903788ad
Jakub Zelenka <bukka@php.net>
2015-05-27 19:03:41 +0100

    Register constant only if not registered already
    This is necessary for older PHP versions

--- php_jsond.h
+++ php_jsond.h
@@ -62,7 +62,6 @@ extern zend_module_entry jsond_module_entry;
 
 #define PHP_JSOND_FUNCTION(jname) PHP_FUNCTION(PHP_JSOND_IDENT(jname))
 #define PHP_JSOND_FE(jname, arginfo) PHP_FE(PHP_JSOND_IDENT(jname), arginfo)
-#define PHP_JSOND_REGISTER_LONG_CONSTANT(name, lval, flags) REGISTER_LONG_CONSTANT(PHP_JSOND_CONSTANT"_"name, lval, flags)
 
 #if (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION == 3 && PHP_RELEASE_VERSION >= 7) || (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION >= 4) || (PHP_MAJOR_VERSION > 5)
 #define PHP_JSOND_FE_END PHP_FE_END
