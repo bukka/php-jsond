5e4cbff00da5321e7016e08854a15c1f0bf9b9bd
Jakub Zelenka <bukka@php.net>
2025-03-29 11:21:10 +0100

    Fix parameters names for PHP functions

--- jsond.stub.php
+++ jsond.stub.php
@@ -3,10 +3,10 @@
 /** @generate-function-entries */
 
 /** @return string|false */
-function jsond_encode(mixed $value, int $options = 0, int $depth = 512): string|false {}
+function jsond_encode(mixed $value, int $flags = 0, int $depth = 512): string|false {}
 
 /** @return mixed */
-function jsond_decode(string $json, ?bool $assoc = null, int $depth = 512, int $options = 0) {}
+function jsond_decode(string $json, ?bool $associative = null, int $depth = 512, int $flags = 0) {}
 
 function jsond_last_error(): int {}
 
